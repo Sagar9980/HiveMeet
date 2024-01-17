b2eaca13698ff2f8c95fd4cd60891e626baf0f81
Sagar Dhami <sagardhami2001@gmail.com>
2024-01-17 22:33:23 +0545

    adde socket server on backend

--- backend/build/server.js
+++ backend/build/server.js
@@ -4,13 +4,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: "http://localhost:3000",
+}));
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
 const port = process.env.PORT || 8000;
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });
-app.listen(port, () => {
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    // Send a welcome message to the connected client
+    socket.emit("message", "Welcome to the Socket.IO server!");
+    // Listen for messages from the client
+    socket.on("chatMessage", (message) => {
+        // Broadcast the message to all connected clients
+        io.emit("message", message);
+    });
+    // Listen for disconnection
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+server.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- backend/server.ts
+++ backend/server.ts
@@ -1,15 +1,48 @@
 import express, { Express, Request, Response } from "express";
+import http from "http";
+import { Server, Socket } from "socket.io";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 const port = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
+io.on("connection", (socket: Socket) => {
+  console.log("A user connected");
 
-app.listen(port, () => {
+  // Send a welcome message to the connected client
+  socket.emit("message", "Welcome to the Socket.IO server!");
+
+  // Listen for messages from the client
+  socket.on("chatMessage", (message: string) => {
+    // Broadcast the message to all connected clients
+    io.emit("message", message);
+  });
+
+  // Listen for disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
