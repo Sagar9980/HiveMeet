44d6c45ebfa057cdeb90887225a475ee5649c51d
Sagar Dhami <sagardhami2001@gmail.com>
2024-01-21 21:34:13 +0545

    fixed necessary packages

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,15 +3,12 @@ import { useEffect } from "react";
 import { io } from "socket.io-client";
 
 export default function Home() {
-  useEffect(() => {
-    const socket = io("http://localhost:8000");
+  const socket = io("http://localhost:8000");
 
-    socket.on("message", (message) => {
-      console.log("Message from server:", message);
-    });
+  socket.on("message", (message) => {
+    console.log("Message from server:", message);
+  });
 
-    // return () => socket.disconnect();
-  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       hello
